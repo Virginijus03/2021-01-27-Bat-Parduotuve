afc05d2b722f774c8e174698fe958c7fae16ef24
Virginijus Klimauskas <klimauskas@yahoo.com>
2021-01-27 18:21:49 +0100

    Add project files.

---
+++ 2021 01 27 BatųParduotuve/Models/Batas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_01_27_BatųParduotuve.Models
+{
+     public class Batas
+    {
+        
+
+        public int BataiId { get; set; }
+        public string BatuPavadinimas { get; set; }
+        public string Moteriski { get; set; }
+        public string Vyriski { get; set; }
+        public string Vaikiski { get; set; }
+        public int BatuKaina { get; set; }
+
+    }
+}
---
+++ 2021 01 27 BatųParduotuve/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_01_27_BatųParduotuve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
